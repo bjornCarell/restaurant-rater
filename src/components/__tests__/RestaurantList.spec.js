610f98fb27e46cc229bb49890575e192ddf1ef2c
Bjorn Carell <bjorn.carell@tink.com>
2021-02-02 07:37:20 +0100

    refactor(test) RestaurantList.spec

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -3,35 +3,33 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
-  // loading behaviour testing
-  it('loads restaurants on first render', () => {
-    // we dont want to connect to our real Redux store
-    // so we create a mock function
-    const loadRestaurants = jest.fn().mockName('loadRestaurants');
-    const restaurants = [];
+  const restaurants = [
+    {id: 1, name: 'Sushi Place'},
+    {id: 2, name: 'Pizza Place'},
+  ];
 
-    render(
+  let loadRestaurants;
+  let context;
+
+  beforeEach(() => {
+    // we dont want to connect to our real Redux store
+    // so we create a mock function - loadRestaurants
+
+    loadRestaurants = jest.fn().mockName('loadRestaurants');
+    context = render(
       <RestaurantList
         loadRestaurants={loadRestaurants}
         restaurants={restaurants}
       />,
     );
+  });
 
+  it('loads restaurants on first render', () => {
     expect(loadRestaurants).toHaveBeenCalled();
   });
 
   it('displays the restaurants', () => {
-    // display behaviour testing
-    const noop = () => {};
-    const restaurants = [
-      {id: 1, name: 'Sushi Place'},
-      {id: 2, name: 'Pizza Place'},
-    ];
-
-    const {queryByText} = render(
-      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
-    );
-
+    const {queryByText} = context;
     expect(queryByText('Sushi Place')).not.toBeNull();
     expect(queryByText('Pizza Place')).not.toBeNull();
   });
