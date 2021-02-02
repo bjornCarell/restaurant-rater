bf31195acc9a33719e1852a078b7739af750be32
Bjorn Carell <bjorn.carell@tink.com>
2021-02-02 07:59:51 +0100

    feat(redux) thunk, mapDispatchToProps RestaurantsList

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import {connect} from 'react-redux';
+import {loadRestaurants} from '../store/restaurants/actions';
 
 export const RestaurantList = ({loadRestaurants, restaurants}) => {
   useEffect(() => {
@@ -19,7 +20,9 @@ const mapStateToProps = state => ({
   restaurant: state.restaurants.records,
 });
 
-export default connect(mapStateToProps)(RestaurantList);
+const mapDispatchToProps = {loadRestaurants};
+
+export default connect(mapStateToProps, mapDispatchToProps)(RestaurantList);
 
 /* 
 
