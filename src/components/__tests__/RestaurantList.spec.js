846a4702b58ab03647e724353162557e618aa18c
Bjorn Carell <bjorn.carell@tink.com>
2021-02-02 07:19:41 +0100

    feat(display restaurants) on first render of RestaurantList

--- src/components/__tests__/RestaurantList.spec.js
+++ src/components/__tests__/RestaurantList.spec.js
@@ -3,13 +3,36 @@ import {render} from '@testing-library/react';
 import {RestaurantList} from '../RestaurantList';
 
 describe('RestaurantList', () => {
+  // loading behaviour testing
   it('loads restaurants on first render', () => {
     // we dont want to connect to our real Redux store
     // so we create a mock function
     const loadRestaurants = jest.fn().mockName('loadRestaurants');
+    const restaurants = [];
 
-    render(<RestaurantList loadRestaurants={loadRestaurants} />);
+    render(
+      <RestaurantList
+        loadRestaurants={loadRestaurants}
+        restaurants={restaurants}
+      />,
+    );
 
     expect(loadRestaurants).toHaveBeenCalled();
+  });
+
+  it('displays the restaurants', () => {
+    // display behaviour testing
+    const noop = () => {};
+    const restaurants = [
+      {id: 1, name: 'Sushi Place'},
+      {id: 2, name: 'Pizza Place'},
+    ];
+
+    const {queryByText} = render(
+      <RestaurantList loadRestaurants={noop} restaurants={restaurants} />,
+    );
+
+    expect(queryByText('Sushi Place')).not.toBeNull();
+    expect(queryByText('Pizza Place')).not.toBeNull();
   });
 });
